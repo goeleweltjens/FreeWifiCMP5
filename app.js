d375dd4313b9b3b1355d0fb3c6da2fac9e487e8b
florerobberechts <s093193@ap.be>
2019-10-15 14:57:05 +0200

    les  14/10

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
-const express = require('express');
-const path = require('path');
+/*
+ * npm init
+ * npm install express --save
+ * npm install request --save
+ * npm install ejs --save
+ *
+ * node(mon) index.js
+ */
 
-const app = express();
-const port = 3000
+var express = require('express');
+var request = require('request');
+var path = require('path');
+
+var app = express();
+
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static('public'));
+
+
+
+app.listen(3000, function() {
+  console.log('Node luistert op poort 3000');
+});
+
+console.log("Webserver draait");
+
+
+request('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek1/MapServer/60/query?where=1%3D1&outFields=*&outSR=4326&f=json',
+  function(error, response, body){
+    var data = JSON.parse(body);
+
+    for(var i=0; i < data.features.length; i++) {
+        console.log("naam: " + data.features[i].attributes.naam);
+
+    }
+
+  }
+);
