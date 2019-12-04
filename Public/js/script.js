2da25c3774ae235ffb498b54fa17ac2aa6b083f5
florerobberechts <s093193@ap.be>
2019-12-05 00:22:04 +0100

    markers groter gezet

--- Public/js/script.js
+++ Public/js/script.js
@@ -14,49 +14,50 @@ var basicmap = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png
 });
 basicmap.addTo(mymap);
 
+
 var pinDefault = L.icon({
     iconUrl: '../images/grijs.png',
-    iconSize:     [17,30],
+    iconSize:     [33,57],
 });
 var pinRed = L.icon({
     iconUrl: '../images/rood.png',
-    iconSize:     [17,30],
+    iconSize:     [33,57],
 });
 var pinYellow = L.icon({
     iconUrl: '../images/geel.png',
-    iconSize:     [17,30],
+    iconSize:     [33,57],
 });
 var pinBlue = L.icon({
     iconUrl: '../images/blauw.png',
-    iconSize:     [17,30],
+    iconSize:     [33,57],
 });
 var pinBordeaux = L.icon({
     iconUrl: '../images/bordeaux.png',
-    iconSize:     [17,30],
+    iconSize:     [33,57],
 });
 
 var pinKaki = L.icon({
     iconUrl: '../images/kaki.png',
-    iconSize:     [17,30],
+    iconSize:     [33,57],
 });
 
 var pinLila = L.icon({
     iconUrl: '../images/lila.png',
-    iconSize:     [17,30],
+    iconSize:     [33,57],
 });
 
 var pinOrange = L.icon({
     iconUrl: '../images/oranje.png',
-    iconSize:     [17,30],
+    iconSize:     [33,57],
 });
 
 var pinPurple = L.icon({
     iconUrl: '../images/paars.png',
-    iconSize:     [17,30],
+    iconSize:     [33,57],
 });
 var pinGreen = L.icon({
     iconUrl: '../images/groen.png',
-    iconSize:     [17,30],
+    iconSize:     [33,57],
 });
 
 
